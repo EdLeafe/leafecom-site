2cd8de2d6393da8fc7acae6e31a53f1caf9bb401
EdLeafe <ed@leafe.com>
2019-01-13 10:56:28 -0600

    Removed stuff that's no longer used.

--- leafecom/tests/functional/test_bitcoincz.py
+++
@@ -1,7 +0,0 @@
-from leafecom.tests import *
-
-class TestBitcoinczController(TestController):
-
-    def test_index(self):
-        response = self.app.get(url(controller='bitcoincz', action='index'))
-        # Test response...
--- leafecom/tests/functional/test_vault.py
+++
@@ -1,7 +0,0 @@
-from leafecom.tests import *
-
-class TestVaultController(TestController):
-
-    def test_index(self):
-        response = self.app.get(url(controller='vault', action='index'))
-        # Test response...
